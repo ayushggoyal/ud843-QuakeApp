52780e9cdfcdb52d8281b95284175dc337c02d0f
ayushggoyal <goyalayush612@gmail.com>
2018-03-18 00:23:58 +0530

    adapter added

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,10 @@
+package com.example.android.quakereport;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by ayush on 17/3/18.
+ */
+
+public class EarthquakeAdapter extends ArrayAdapter {
+}
